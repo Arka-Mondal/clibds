6a034bc8d6a67709e781f814f8d08f53d1eb2d50
Arka Mondal <arkamondalofficial@gmail.com>
2022-09-19 16:35:20 +0530

    Added LICENSE description

--- Queue/queue.h
+++ Queue/queue.h
@@ -1,3 +1,9 @@
+/*
+  Copyright (c) 2022, Arka Mondal. All rights reserved.
+  Use of this source code is governed by a BSD-style license that
+  can be found in the LICENSE file.
+*/
+
 #ifndef QUEUE_H
   #define QUEUE_H
 
